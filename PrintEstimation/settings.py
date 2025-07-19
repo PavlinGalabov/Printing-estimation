bdbca0cc52930c89296981c1110428867295ce75
PavlinGalabov <pavlin.galabov@gmail.com>
2025-07-20 00:10:52 +0300

    feat: initialize Django project with professional structure

--- PrintEstimation/settings.py
+++ PrintEstimation/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PrintEstimation.accounts',
+    'PrintEstimation.core',
+    'PrintEstimation.jobs',
+    'PrintEstimation.operations',
+
+
 ]
 
 MIDDLEWARE = [
